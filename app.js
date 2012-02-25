56a2487958f3bcfd64f8f4a4325e00a3298bf4bc
Emilio Facundo Cabrera <cabrerafacundo@gmail.com>
2012-02-25 18:41:53 -0300

    It's the right way to use the History.js / History API. Removed duplicated view updates / initial push state.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
+$(function(){
   //
   // Update view
   //
   function updateContent(data) {
-    if (data === null) {
-      return;
+    if (data === null || $.isEmptyObject(data)) {
+      data = {
+      	content: "Welcome to Kittens! ... more text",
+      	photo:"http://placekitten.com/300/300"
+      };
     }
+    
+    console.log('updateContent: ', data);
+    
     // update the message & the photo
     $('#content').html(data.content);
     $('#photo').attr('src', data.photo);
@@ -21,34 +27,22 @@ $(document).ready(function(){
     promise = $.getJSON('json/' + key + '.json');
 
     promise.done(function(data) {
-      // Update the view.
-      updateContent(data);
-      
+    
       // Store the AJAX response in the History
+      // fires the statechange event, so, Don't fire the view update here!
+      // just use the statechange event as a view update handler.
       History.pushState(data, document.title, event.target.href);
+
     });
     
     return false;
   });
 
   //
-  // Initial setup because after change the content using javascript we need 
-  // to rebuilt it.
-  //
-  History.pushState({
-      content: "Welcome to Kittens! ... more text",
-      photo:"http://placekitten.com/300/300"
-    }, document.title, '/'
-  );
-
-  // 
   // Bind to StateChange Event
   //
   History.Adapter.bind(window,'statechange',function() { // Note: We are using statechange instead of popstate
     var State = History.getState(); // Note: We are using History.getState() instead of event.state
-    
-    console.log('statechange data: ', State.data);
-
     updateContent(State.data);
   });
 });
